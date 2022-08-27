0fea6be17a0ff5b6765d8aa35edbdc0049730d97
Jacob Yancey <jyancey@centretechnologies.com>
2022-08-26 19:16:09 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input First Number to add");
+            try
+            {
+
+                int firstNum = int.Parse(Console.ReadLine());
+                Console.WriteLine("Input Second Number");
+                int secondNum = int.Parse(Console.ReadLine());
+
+                while (true)
+                {
+                    Console.WriteLine("Do you want to multiply, divide, add, or subtract two numbers?");
+                    string answer = Console.ReadLine();
+
+
+                    if (answer == "multiply")
+                    {
+                        Console.WriteLine(firstNum * secondNum);
+                        return;
+
+                    }
+                    if (answer == "divide")
+                    {
+                        Console.WriteLine(firstNum / secondNum);
+                        return;
+
+                    }
+                    if (answer == "add")
+                    {
+                        Console.WriteLine(firstNum + secondNum);
+                        return;
+
+                    }
+                    if (answer == "subtract")
+                    {
+                        Console.WriteLine(firstNum - secondNum);
+                        return;
+                    }
+                    Console.WriteLine("Word not matching anything, please try again");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error Occured");
+            }
+
+        }
+
+
+
+
+    }
+}
